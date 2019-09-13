fb321f7ca011e7e0b83fcde1af8e6188aafa9746
unknown <isc.orangelc@gmail.com>
2019-09-13 16:09:38 -0500

    modify sintaxis

--- example.tf
+++ example.tf
@@ -5,8 +5,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
+  ami           = "ami-2757f631",
+  instance_type = "t2.micro",
   tags {
     Name = "test-instance-terraform"
   }
